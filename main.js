6cc2b7ca7ef3e893ba3f69e75cb687c5861a096a
Dongyu Zheng <garydzheng@gmail.com>
2015-11-14 18:15:07 -0500

    restructure

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+
+  console.log(getSpendings("20561724","1237","3/1/2014","11/11/2015"));
+
+});
--- transactions.js
+++ transactions.js
@@ -60,10 +60,3 @@ function getSpendings(id,pin,from,to) {
   });
 
 }
-
-
-$(document).ready(function() {
-
-  console.log(getSpendings("20561724","1237","3/1/2014","11/11/2015"));
-
-});
