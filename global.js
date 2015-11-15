36f09066f9f1094c0bab10770d2e494e48c721cf
Gary Zheng <garydzheng@gmail.com>
2015-11-15 11:05:36 -0500

    removed Balance For:

--- global.js
+++ global.js
@@ -247,6 +247,18 @@ function generateBasicOptions() {
                 series: {
                     cursor: 'pointer'
                 },
+                fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                },
                 stickyTracking : true
             }     
         },
